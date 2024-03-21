db9fcbf0a0eb1a7ba155c0f8f9a9dca1331f2c6e
Sonal Gond <sonalgond132@gmail.com>
2024-03-21 18:33:56 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './Home.js'
+
+const App = () => {
+  console.log('dishsshsh');
+  return (
+    <div>
+      <Home/>
+      
+      
+    </div>
+  )
+}
+
+export default App;
